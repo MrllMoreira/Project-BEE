a30b471da485acab89c033d7a1abfe75c74c2e4d
Luan Ibanhez <luanibanhez@gmail.com>
2025-10-17 14:20:59 -0300

    começo do layou

---
+++ resources/views/livewire/index.blade.php
@@ -0,0 +1,66 @@
+<div class="  p-16 h-[780px]">
+    <h1 class="mb-4 text-3xl font-bold">EMEF Dr. Carlos de Almeida</h1>
+    <div class="flex items-center justify-center h-full">
+        <div class="w-full h-full p-4 bg-gray-200">
+           <div class="flex items-center justify-center w-full gap-6">
+            <hr class="w-40 border-gray-400">
+            <h2 class="text-xl font-semibold ">Patrimonios</h2>
+            <hr class="w-40 col-span-2 border-gray-400">
+           </div>
+
+
+           {{-- <div class="grid grid-cols-12 gap-20 px-20 mt-4">
+                <div class="grid grid-cols-12 col-span-4 ">
+                    <div class="col-span-12 bg-white border border-gray-200 rounded-md shadow-lg h-28">
+
+                    </div>
+                    <div class="col-span-4">
+
+                    </div>
+                    <div class="flex items-center justify-center h-10 col-span-4 gap-1 mt-3 text-xs text-gray-700 bg-white border border-gray-200 rounded-md shadow-lg cursor-pointer hover:bg-gray-100 ">
+                        <p class="">30 dias</p>
+                        <x-ts-icon icon="arrow-down" class="w-4 font-black"/>
+
+                    </div>
+                </div>
+
+
+               <div class="col-span-4 bg-white border border-gray-200 rounded-md shadow-lg h-28">
+
+               </div>
+               <div class="col-span-4 bg-white border border-gray-200 rounded-md shadow-lg h-28">
+
+               </div>
+           </div> --}}
+
+           <div class="flex items-center justify-around w-full mt-8">
+            <div class="w-[350px] flex gap-3 flex-col justify-center items-center">
+                <div class="w-[350px] h-24 bg-white">
+
+                </div>
+                <div class="w-[100px] h-10 bg-white flex items-center justify-center gap-1 hover:font-semibold rounded-md hover:bg-gray-100 cursor-pointer focus:bg-gray-100 focus:font-semibold">
+                    30 dias <x-ts-icon class="w-4 h-4" icon="arrow-down"/>
+                </div>
+            </div>
+            <div class="w-[350px] flex gap-3 flex-col justify-center items-center">
+                <div class="w-[350px] h-24 bg-white">
+
+                </div>
+                <div class="w-[100px] h-10 bg-white flex items-center justify-center gap-1 hover:font-semibold rounded-md hover:bg-gray-100 cursor-pointer focus:bg-gray-100 focus:font-semibold">
+                    30 dias <x-ts-icon class="w-4 h-4" icon="arrow-down"/>
+                </div>
+            </div>
+            <div class="w-[350px] flex gap-3 flex-col justify-center items-center">
+                <div class="w-[350px] h-24 bg-white">
+
+                </div>
+                <div class="w-[100px] h-10 bg-white flex items-center justify-center gap-1 hover:font-semibold rounded-md hover:bg-gray-100 cursor-pointer focus:bg-gray-100 focus:font-semibold">
+                    30 dias <x-ts-icon class="w-4 h-4" icon="arrow-down"/>
+                </div>
+            </div>
+           </div>
+
+        </div>
+
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,21 @@
 <?php
 
+use App\Livewire\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+
+Route::get('/dashboard', Index::class)->name('dashboard');
