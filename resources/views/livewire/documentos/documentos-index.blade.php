9b954e3ab14ee41effa7e926a172b649974d42e9
edualnd <ifeduarda.luiz@gmail.com>
2025-11-04 20:32:01 -0300

    add: overflow nos tabs

--- resources/views/livewire/documentos/documentos-index.blade.php
+++ resources/views/livewire/documentos/documentos-index.blade.php
@@ -1,16 +1,24 @@
 <div class="flex lg:flex-row flex-col gap-4 p-2">
+    @php
+        $labelClass = "absolute inset-0 cursor-pointer flex items-center pl-2";
+        $liClass = "min-h-[30px] h-[30px] max-h-[30px] rounded-xl pl-2 h-7 relative";
+        $divClass = "overflow-y-scroll py-2 max-h-56";
+        $ulClass ="flex flex-col justify-center gap-2";
+    @endphp
     <x-ts-tab  wire:model.live="tab"  x-on:selected-change="$wire.set('selectedDocument', '')">
         <x-ts-tab.items tab="Assinar" >
-            <ul class="flex flex-col justify-center gap-2  border-am">
+            <div class="{{ $divClass }}">
+                <ul class="{{ $ulClass }}">
                 @foreach ($documentos as $doc)
                     @if ($doc['status'] == "assinar")
-                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}  rounded-xl pl-2 h-7 relative">
+                        <li class="{{ $liClass }} {{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}">
                             <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
                             <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label>
                         </li>
                     @endif
                 @endforeach
             </ul>
+            </div>
             @if ($selectedDocument != "")
                 <div class="flex justify-end items-center mt-3">
                     <x-button >Assinar</x-button>  
@@ -18,31 +26,37 @@
             @endif
         </x-ts-tab.items>
         <x-ts-tab.items tab="Em espera">
-            <ul class="flex flex-col justify-center gap-2 ">
+            <div class="{{ $divClass }}">
+                <ul class="{{ $ulClass }}">
                 @foreach ($documentos as $doc)
                     @if ($doc['status'] == "em espera")
-                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}  rounded-xl pl-2 h-7 relative">
+                        <li class="{{ $liClass }} {{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}">
                             <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
-                            <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label
+                            <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label>
                         </li>
                     @endif
                 @endforeach
             </ul>
+            </div>
+            
         </x-ts-tab.items>
         <x-ts-tab.items tab="Assinados">
-            <ul class="flex flex-col justify-center gap-2 ">
-                @foreach ($documentos as $doc)
-                    @if ($doc['status'] == "assinado")
-                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}  rounded-xl pl-2 h-7 relative">
-                            <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
-                            <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label
-                        </li>
-                    @endif
-                @endforeach
-            </ul>
+            <div class="{{ $divClass }}">
+                <ul class="{{ $ulClass }}">
+                    @foreach ($documentos as $doc)
+                        @if ($doc['status'] == "assinado")
+                            <li class= "{{ $liClass }}  {{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}">
+                                <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
+                                <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label>
+                            </li>
+                        @endif
+                    @endforeach
+                </ul>
+            </div>
+            
         </x-ts-tab.items>
 
     </x-ts-tab>
     <iframe src="..." width="100%" height="800px"></iframe>
-
+    
 </div>
