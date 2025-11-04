bc37dee75857e9f09916e214cc3190ec4eab7c19
Eduarda-Ubuntu <ifeduarda.luiz@gmail.com>
2025-11-04 11:15:42 -0300

    add: updateTab no componente,  reseta o documento selecionado ao trocar de tab

--- app/Livewire/Documentos/DocumentosIndex.php
+++ app/Livewire/Documentos/DocumentosIndex.php
@@ -9,38 +9,42 @@ class DocumentosIndex extends Component
     public $tab = "Assinar";
     public $selectedDocument = "";
     public $documentos = [
-                    [
-                        "id" => 1,
-                        "nome" => "Contrato de Empréstimo Pessoal",
-                        "tipo" => "Empréstimo",
-                        "status" => "assinar"
-                    ],
-                    [
-                        "id" => 2,
-                        "nome" => "Termo de Adesão - Cartão de Crédito", 
-                        "tipo" => "Cartão",
-                        "status" => "assinado"
-                    ],
-                    [
-                        "id" => 3,
-                        "nome" => "Contrato de Financiamento Veicular",
-                        "tipo" => "Financiamento",
-                        "status" => "em espera"
-                    ],
-                    [
-                        "id" => 4,
-                        "nome" => "Contrato de Abertura de Conta",
-                        "tipo" => "Conta Bancária",
-                        "status" => "assinar"
-                    ],
-                    [
-                        "id" => 5,
-                        "nome" => "Termo de Portabilidade de Crédito",
-                        "tipo" => "Portabilidade",
-                        "status" => "em espera"
-                    ]
-                ];
+            [
+                "id" => 1,
+                "nome" => "Contrato de Empréstimo Pessoal",
+                "tipo" => "Empréstimo",
+                "status" => "assinar"
+            ],
+            [
+                "id" => 2,
+                "nome" => "Termo de Adesão - Cartão de Crédito", 
+                "tipo" => "Cartão",
+                "status" => "assinado"
+            ],
+            [
+                "id" => 3,
+                "nome" => "Contrato de Financiamento Veicular",
+                "tipo" => "Financiamento",
+                "status" => "em espera"
+            ],
+            [
+                "id" => 4,
+                "nome" => "Contrato de Abertura de Conta",
+                "tipo" => "Conta Bancária",
+                "status" => "assinar"
+            ],
+            [
+                "id" => 5,
+                "nome" => "Termo de Portabilidade de Crédito",
+                "tipo" => "Portabilidade",
+                "status" => "em espera"
+            ]
+        ];
     
+    public function updatedTab($value){
+        $this->selectedDocument = "";
+    }
+
     public function render()
     {
         return view('livewire.documentos.documentos-index');
--- resources/views/livewire/documentos/documentos-index.blade.php
+++ resources/views/livewire/documentos/documentos-index.blade.php
@@ -1,10 +1,10 @@
 <div class="flex lg:flex-row flex-col gap-4 p-2">
-    <x-ts-tab  wire:model.live="tab">
+    <x-ts-tab  wire:model.live="tab"  x-on:selected-change="$wire.set('selectedDocument', '')">
         <x-ts-tab.items tab="Assinar" >
             <ul class="flex flex-col justify-center gap-2  border-am">
                 @foreach ($documentos as $doc)
                     @if ($doc['status'] == "assinar")
-                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : ''}}  rounded-xl pl-2 h-7 hover:bg-slate-100 relative">
+                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}  rounded-xl pl-2 h-7 relative">
                             <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
                             <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label>
                         </li>
@@ -21,7 +21,7 @@
             <ul class="flex flex-col justify-center gap-2 ">
                 @foreach ($documentos as $doc)
                     @if ($doc['status'] == "em espera")
-                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : ''}}  rounded-xl pl-2 h-7 hover:bg-slate-100 relative">
+                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}  rounded-xl pl-2 h-7 relative">
                             <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
                             <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label
                         </li>
@@ -33,7 +33,7 @@
             <ul class="flex flex-col justify-center gap-2 ">
                 @foreach ($documentos as $doc)
                     @if ($doc['status'] == "assinado")
-                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : ''}}  rounded-xl pl-2 h-7 hover:bg-slate-100 relative">
+                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : 'hover:bg-slate-100'}}  rounded-xl pl-2 h-7 relative">
                             <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
                             <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label
                         </li>
