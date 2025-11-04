b00668565ccf27e4be8c2ddf7fb173d9b94e0b5b
Eduarda-Ubuntu <ifeduarda.luiz@gmail.com>
2025-11-04 11:08:42 -0300

    add: blade de documentos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use TallStackUi\Facades\TallStackUi;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TallStackUi::personalize('tab')
+            ->block('item.select', 'bg-white text-amber-600 border-b-2 border-amber-500 font-medium');          
     }
 }
--- resources/views/livewire/documentos/documentos-index.blade.php
+++ resources/views/livewire/documentos/documentos-index.blade.php
@@ -1,3 +1,48 @@
-<div>
-    {{-- Be like water. --}}
+<div class="flex lg:flex-row flex-col gap-4 p-2">
+    <x-ts-tab  wire:model.live="tab">
+        <x-ts-tab.items tab="Assinar" >
+            <ul class="flex flex-col justify-center gap-2  border-am">
+                @foreach ($documentos as $doc)
+                    @if ($doc['status'] == "assinar")
+                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : ''}}  rounded-xl pl-2 h-7 hover:bg-slate-100 relative">
+                            <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
+                            <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label>
+                        </li>
+                    @endif
+                @endforeach
+            </ul>
+            @if ($selectedDocument != "")
+                <div class="flex justify-end items-center mt-3">
+                    <x-button >Assinar</x-button>  
+                </div>
+            @endif
+        </x-ts-tab.items>
+        <x-ts-tab.items tab="Em espera">
+            <ul class="flex flex-col justify-center gap-2 ">
+                @foreach ($documentos as $doc)
+                    @if ($doc['status'] == "em espera")
+                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : ''}}  rounded-xl pl-2 h-7 hover:bg-slate-100 relative">
+                            <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
+                            <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label
+                        </li>
+                    @endif
+                @endforeach
+            </ul>
+        </x-ts-tab.items>
+        <x-ts-tab.items tab="Assinados">
+            <ul class="flex flex-col justify-center gap-2 ">
+                @foreach ($documentos as $doc)
+                    @if ($doc['status'] == "assinado")
+                        <li class="{{$doc['id'] == $selectedDocument ? 'bg-slate-300' : ''}}  rounded-xl pl-2 h-7 hover:bg-slate-100 relative">
+                            <input type="radio" name="documento" id="{{$doc['id']}}" wire:model.live="selectedDocument" value="{{$doc['id']}}" class="hidden">
+                            <label class="absolute inset-0 cursor-pointer flex items-center pl-2" for="{{$doc['id']}}">{{$doc['nome']}}</label
+                        </li>
+                    @endif
+                @endforeach
+            </ul>
+        </x-ts-tab.items>
+
+    </x-ts-tab>
+    <iframe src="..." width="100%" height="800px"></iframe>
+
 </div>
